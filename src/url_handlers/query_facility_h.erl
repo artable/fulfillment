d2bdb1a207720763619963f7c79033bc5884f037
Logan Butler <logan3528@gmail.com>
2023-03-24 21:17:14 -0600

    handlers updated to use otp_wrapper

--- src/url_handlers/query_facility_h.erl
+++ src/url_handlers/query_facility_h.erl
@@ -2,15 +2,6 @@
 
 -export([init/2]).
 
-init(Req0, Opts) ->
-	{ok,Data,_} = cowboy_req:read_body(Req0),
-	%it is expected that the data consists of one quoted-string name
-	%in an array.
-	[Name|_] = jsx:decode(Data),
-    Worker_PID = distributor:get_worker(fac_dist, Name),
-	City = jsx:encode(query_facility:get_city_of(Name,Worker_PID)),
-	%io:format("~p~n",[get_friends_server:get_friends_of(Name)]),
-	Req = cowboy_req:reply(200, #{
-		<<"content-type">> => <<"text/json">>
-	}, City, Req0),
-	{ok, Req, Opts}.
+init(Req8, Distributor) ->
+    Req = otp_wrapper:push_query(Req8,Distributor),
+	{ok, Req, Distributor}.
