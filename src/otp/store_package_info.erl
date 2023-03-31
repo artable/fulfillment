3fd029b717c5ed96b746c533dc6ed878a496fe49
Logan Butler <logan3528@gmail.com>
2023-03-30 21:17:54 -0600

    removed dist add at worker init

--- src/otp/store_package_info.erl
+++ src/otp/store_package_info.erl
@@ -75,7 +75,6 @@ put_package(Data, PID) -> gen_server:call(PID, {put, Data}).
 %%--------------------------------------------------------------------
 -spec init(term()) -> {ok, term()}|{ok, term(), number()}|ignore |{stop, term()}.
 init(Dist) ->
-    distributor:add(Dist),
     case riakc_pb_socket:start_link("143.198.108.90", 8087) of 
         {ok,Riak_Pid} -> {ok,Riak_Pid};
         _ -> {stop,link_failure}
