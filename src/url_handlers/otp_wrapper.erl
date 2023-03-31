cf10896116ce3f99e9d34473e830e9df793e5281
Logan Butler <logan3528@gmail.com>
2023-03-30 21:38:37 -0600

    dist pid pattern matching

--- src/url_handlers/otp_wrapper.erl
+++ src/url_handlers/otp_wrapper.erl
@@ -7,7 +7,7 @@ store_query(Req0, [Distributor]) ->
     Holder_UUID = maps:get(<<"holder_uuid">>, Map),
     Pack_UUID = maps:get(<<"package_uuid">>, Map),
     Time = maps:get(<<"time_stamp">>, Map),
-    {ok, Worker} = distributor:call(Distributor),
+    {ok, {global, Worker}} = distributor:call(Distributor),
     case gen_server:call(Worker, {put, {Pack_UUID, Holder_UUID, Time}}) of
         fail -> Req0;
         _ -> cowboy_req:reply(200, Req0)
@@ -19,14 +19,14 @@ store_query_veh(Req0, [Distributor]) ->
     Location = maps:get(<<"location">>, Map),
     UUID = maps:get(<<"vehicle_uuid">>, Map),
     Time = maps:get(<<"time_stamp">>, Map),
-    {ok, PID} = distributor:call(Distributor),
+    {ok, {global, PID}} = distributor:call(Distributor),
     case gen_server:call(PID, {get, {UUID, Location, Time}}) of
         fail -> Req0;
         _ -> cowboy_req:reply(200, Req0)
     end.
 
 push_query(Req0, [Distributor]) ->
-    {ok,Worker} = distributor:call(Distributor),
+    {ok,{global, Worker}} = distributor:call(Distributor),
     {ok,Data,_} = cowboy_req:read_body(Req0),
     Map = jsx:decode(Data),
     case maps:is_key(<<"package_uuid">>, Map) of
